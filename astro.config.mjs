8051d88728e1239b5c60044404bace3f27988c3a
magestein <hectorsteiner@gmail.com>
2024-06-10 21:32:34 -0400

    `refactor: Update Astro configuration to remove unnecessary import`

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import auth from "auth-astro";
 
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
